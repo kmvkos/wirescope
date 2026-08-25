26851bf231bbfbb1a8d11b609f69a2af957085d5
kmvkos <32267921+kmvkos@users.noreply.github.com>
2026-08-25 13:04:09 +0300

    M10: add traffic analysis package

---
+++ traffic_analysis/__init__.py
@@ -0,0 +1,6 @@
+"""Deterministic analysis of retained packet captures."""
+
+from traffic_analysis.analyzer import TrafficAnalyzer
+from traffic_analysis.render import render_markdown, render_text
+
+__all__ = ["TrafficAnalyzer", "render_markdown", "render_text"]

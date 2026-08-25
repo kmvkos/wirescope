9b2233838ad1fd563705009db2281f9bc0c8af88
kmvkos <32267921+kmvkos@users.noreply.github.com>
2026-08-25 19:54:41 +0300

    M11.3: register SNMP topology handler export

--- jobs/handlers/__init__.py
+++ jobs/handlers/__init__.py
@@ -6,6 +6,7 @@ from jobs.handlers.findings import FindingsEvaluationHandler
 from jobs.handlers.passive import PassiveDiscoveryHandler
 from jobs.handlers.protocol import ProtocolAuditHandler
 from jobs.handlers.report import ReportGenerationHandler
+from jobs.handlers.snmp_topology import SnmpTopologyHandler
 from jobs.handlers.traffic import TrafficAnalysisHandler
 
 __all__ = [
@@ -15,5 +16,6 @@ __all__ = [
     "PassiveDiscoveryHandler",
     "ProtocolAuditHandler",
     "ReportGenerationHandler",
+    "SnmpTopologyHandler",
     "TrafficAnalysisHandler",
 ]

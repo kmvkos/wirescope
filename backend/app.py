8ef6460655789eaf72d9c3b06474a90a3151f56b
kmvkos <32267921+kmvkos@users.noreply.github.com>
2026-08-27 15:25:36 +0300

    Frontend: load PCAP management controls

--- backend/app.py
+++ backend/app.py
@@ -36,6 +36,7 @@ _TOPOLOGY_HARDENING_ASSET_VERSION = "20260826-ui17"
 _SSH_TOPOLOGY_ASSET_VERSION = "20260826-ui18"
 _GLOBAL_ANALYSIS_ASSET_VERSION = "20260827-ui19"
 _PRODUCT_COHERENCE_ASSET_VERSION = "20260827-ui26"
+_PCAP_MANAGEMENT_ASSET_VERSION = "20260827-ui27"
 
 
 def create_app(
@@ -217,6 +218,7 @@ def create_app(
                 f'<script src="/static/report_management.js?v={_UI_ASSET_VERSION}"></script>\n'
                 f'<script src="/static/audit_management.js?v={_UI_ASSET_VERSION}"></script>\n'
                 f'<script src="/static/traffic_analysis.js?v={_UI_ASSET_VERSION}"></script>\n'
+                f'<script src="/static/pcap_management.js?v={_PCAP_MANAGEMENT_ASSET_VERSION}"></script>\n'
                 f'<script src="/static/topology.js?v={_UI_ASSET_VERSION}"></script>\n'
                 f'<script src="/static/topology_hardening.js?v={_TOPOLOGY_HARDENING_ASSET_VERSION}"></script>\n'
                 f'<script src="/static/topology_compare.js?v={_TOPOLOGY_HISTORY_ASSET_VERSION}"></script>\n'
